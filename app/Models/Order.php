7ae54d25693b35285ad8ff36536c393c719a196a
wael kasouha <waelkas99@gmail.com>
2023-09-28 14:29:36 -0400

    make some migrations and models

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_price',
+    ];
+
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'id', 'user_id');
+    }
+}
