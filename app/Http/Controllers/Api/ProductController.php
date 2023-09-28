6f6d4413c5ac1a89fa76a95a64ea26fa45466791
wael kasouha <waelkas99@gmail.com>
2023-09-28 12:51:16 -0400

    ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -12,7 +14,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::latest()->paginate(15);
+
+        return ProductResource::collection($products);
     }
 
     /**
@@ -20,7 +24,45 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the request
+        $request->validate([
+            'name' => 'required|string',
+            'slug_name' => 'required|string|unique:products,slug',
+            'description' => 'required|text',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+            'cover_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Create a new product
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->slug = $request->input('slug');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->quantity = $request->input('quantity');
+
+        // Upload the cover image
+        $coverImage = $request->file('cover_image');
+        $coverImageName = uniqid() . '.' . $coverImage->getClientOriginalExtension();
+        $coverImage->storeAs('products/covers', $coverImageName);
+
+        // Set the cover image name
+        $product->cover_image = $coverImageName;
+
+        // Save the product
+        $product->save();
+
+        // Upolad Products Images
+        // foreach ($request->file('images') as $image) {
+        //     $path = uploadImage('products/images', $image);
+        //     $product->images()->update([
+        //          'url' => $path,
+        //     ]);
+        // }
+
+        // Redirect to the product list page
+        return redirect()->route('products.index');
     }
 
     /**
@@ -28,7 +70,9 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        return new ProductResource($product);
     }
 
     /**
@@ -36,7 +80,16 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $product = Product::find($id);
+
+        // if ($product) {
+        //     if ($request->hasFile('cover_image')) {
+        //         //delete old cover
+        //         deleteImage($product->cover_image);
+        //         //upload new cover
+        //         uploadImage('products/images', $request->file('cover_image'));
+        //     }
+        // }
     }
 
     /**
@@ -44,6 +97,7 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::find($id);
+        $product->delete($id);
     }
 }
